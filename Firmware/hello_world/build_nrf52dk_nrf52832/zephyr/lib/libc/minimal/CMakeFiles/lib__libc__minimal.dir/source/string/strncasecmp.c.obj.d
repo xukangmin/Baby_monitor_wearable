7245235c7de7b9f53b7d931d2235e6666e867eef
Kangmin <xukangmin@gmail.com>
2021-03-17 13:50:28 -0500

    u

--- Firmware/hello_world/build_nrf52dk_nrf52832/zephyr/lib/libc/minimal/CMakeFiles/lib__libc__minimal.dir/source/string/strncasecmp.c.obj.d
+++
@@ -1,7 +0,0 @@
-zephyr/lib/libc/minimal/CMakeFiles/lib__libc__minimal.dir/source/string/strncasecmp.c.obj: \
- C:/Users/kangmin/ncs/v1.5.0/zephyr/lib/libc/minimal/source/string/strncasecmp.c \
- C:/Users/kangmin/Documents/GitHub/Baby_monitor_wearable/Firmware/hello_world/build_nrf52dk_nrf52832/zephyr/include/generated/autoconf.h \
- C:/Users/kangmin/ncs/v1.5.0/zephyr/include/toolchain/zephyr_stdint.h \
- C:/Users/kangmin/ncs/v1.5.0/zephyr/lib/libc/minimal/include/stdlib.h \
- c:\users\kangmin\ncs\v1.5.0\toolchain\opt\lib\gcc\arm-none-eabi\9.2.1\include\stddef.h \
- C:/Users/kangmin/ncs/v1.5.0/zephyr/lib/libc/minimal/include/ctype.h
--- Firmware/hello_world/build_nrf52dk_nrf52832/zephyr/lib/libc/minimal/CMakeFiles/lib__libc__minimal.dir/source/string/strspn.c.obj.d
+++
@@ -1,7 +0,0 @@
-zephyr/lib/libc/minimal/CMakeFiles/lib__libc__minimal.dir/source/string/strspn.c.obj: \
- C:/Users/kangmin/ncs/v1.5.0/zephyr/lib/libc/minimal/source/string/strspn.c \
- C:/Users/kangmin/Documents/GitHub/Baby_monitor_wearable/Firmware/hello_world/build_nrf52dk_nrf52832/zephyr/include/generated/autoconf.h \
- C:/Users/kangmin/ncs/v1.5.0/zephyr/include/toolchain/zephyr_stdint.h \
- C:/Users/kangmin/ncs/v1.5.0/zephyr/lib/libc/minimal/include/string.h \
- c:\users\kangmin\ncs\v1.5.0\toolchain\opt\lib\gcc\arm-none-eabi\9.2.1\include\stddef.h \
- C:/Users/kangmin/ncs/v1.5.0/zephyr/lib/libc/minimal/include/bits/restrict.h
